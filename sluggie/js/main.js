de7fc9d8c90cbc56383f73c862d823620675f99c
Tyler Van Hoomissen <linuxsable@gmail.com>
2010-11-12 00:25:09 -0800

    updated for jquery

--- sluggie/js/main.js
+++ sluggie/js/main.js
@@ -1,8 +1,8 @@
-var canvas = document.getElementById('canvas');
+var canvas = $('#canvas');
 
 if (canvas && canvas.getContext) {
     canvasContext = canvas.getContext('2d');
-    canvasBuffer = document.createElement('canvas');
+    canvasBuffer = $(document).createElement('canvas');
     canvasBuffer.width = canvas.width;
     canvasBuffer.height = canvas.height;
     canvasBufferContext = canvasBuffer.getContext('2d');
