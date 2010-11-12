d3941d7d442f2392542bb25e7d2b9820f92174d1
Tyler Van Hoomissen <linuxsable@gmail.com>
2010-11-11 23:31:42 -0800

    base structore

---
+++ sluggie/js/main.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('canvas');
+
+if (canvas && canvas.getContext) {
+    canvasContext = canvas.getContext('2d');
+    canvasBuffer = document.createElement('canvas');
+    canvasBuffer.width = canvas.width;
+    canvasBuffer.height = canvas.height;
+    canvasBufferContext = canvasBuffer.getContext('2d');
+}
